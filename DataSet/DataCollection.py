d579f3b2a9b35c5598696d2745f689ad2f32ae5a
Divya5595 <divya5595@gmail.com>
2017-03-17 22:33:59 +0530

    Add files via upload
    Automating for 5 companies 
    Ending date: 30th Jan 2017
    12 datasets for each company with a difference of 4 weeks
    And one output for 31st Jan 2017
    
    total 49 files get created!
    Output Path has to be changed for every person

---
+++ DataSet/DataCollection.py
@@ -0,0 +1,26 @@
+#Code to get Data
+#For 5 companies
+#With a gap of 4 weeks each, keeping the end date const, changing start time
+
+import datetime
+from pandas_datareader import data
+
+start_date = datetime.datetime(2017,1,2)
+stockstoPull =  'ONGC.NS', 'SBI', 'TCS.NS' , 'ONGC.BO', 'BHEL.BO'
+
+for stockTicker in stockstoPull:
+    print ("Pulling Data of " + stockTicker)
+    for i in range(12): 
+        start_date += datetime.timedelta(weeks=-4)
+        print(i+1)
+        df = data.DataReader(stockTicker, 'yahoo', start_date, '2017-01-30')
+        abc = 'C:\Users\Divya K\Desktop\DataSets\\' + str(i+1)+ 'month' + stockTicker.partition('.')[0] + '.csv'
+        #print (abc)
+        df.to_csv(abc)
+
+for stockTicker in stockstoPull:
+    df = data.DataReader(stockTicker, 'yahoo', '2017-01-05', '2017-01-05')
+    abc = 'C:\Users\Divya K\Desktop\DataSets\\' + '00Output'+  '.csv'
+    #print (abc)
+    df.to_csv(abc)
+
